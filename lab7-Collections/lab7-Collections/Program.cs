8613d2a55bd464c118cc4f2f49f87774afd2aafc
xia LIU <xia.liu2@outlook.com>
2019-01-16 07:46:07 -0800

    setup files

---
+++ lab7-Collections/lab7-Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab7_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
